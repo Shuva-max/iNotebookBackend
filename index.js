e28073c249c3e289578e715f412599f89fa1399e
Shuva Paul <shuva575paul@gmail.com>
2023-10-07 19:17:32 +0530

    setting our start command for backend

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const connnectToMongo = require('./db');
 const express = require('express');
 const cors = require('cors')
+require('dotenv').config()
 
 connnectToMongo();
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 app.use(cors());
 
 app.use(express.json());
